0b7e2c3e7fbb88e10795bf6cae7c0f3b48d55555
root <root@c118c6cef6bb>
2020-10-23 01:27:02 +0000

    fdae

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,33 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
-Thread[]
+private Thread[] threads;
+private ConcurrentLinkedQueue<Task> taskQueue;
+public ThreadPool(int totalThreads) {
+	
+	threads=new Thread[totalThreads];
+	for (int i = 0; i < threads.length; i++) {
+		Worker worker=new Worker();
+		threads[i]= new Thread(worker);
+	}
+	taskQueue=new ConcurrentLinkedQueue<Task>();
+}
+public void addTask(Task task) {
+	taskQueue.add(task);
+}
+public void start() {
+ for (int i = 0; i < threads.length; i++) {
+	threads[i].start();
+	
+}
+ for (int i = 0; i < threads.length; i++) {
+	 try {
+			threads[i].join();
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+}
+}
 }
